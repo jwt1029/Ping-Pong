fbfdf46c5db8a1c00571c8e5a2f776d413438ea4
graykick <graykick@naver.com>
2017-04-07 12:21:50 +0900

    add game room
    add game room

---
+++ PingPong/register.Designer.cs
@@ -0,0 +1,154 @@
+﻿namespace PingPong
+{
+    partial class register
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(209, 99);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(199, 21);
+            this.textBox1.TabIndex = 0;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(209, 138);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.PasswordChar = '*';
+            this.textBox2.Size = new System.Drawing.Size(199, 21);
+            this.textBox2.TabIndex = 1;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(209, 174);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.PasswordChar = '*';
+            this.textBox3.Size = new System.Drawing.Size(199, 21);
+            this.textBox3.TabIndex = 2;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(437, 99);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(107, 96);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "회원가입";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(166, 104);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(16, 12);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "ID";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(141, 143);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(61, 12);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "password";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(107, 179);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(97, 12);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "passwordCheck";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(141, 62);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(63, 12);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Username";
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(209, 59);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(199, 21);
+            this.textBox4.TabIndex = 7;
+            this.textBox4.TextChanged += new System.EventHandler(this.textBox4_TextChanged);
+            // 
+            // register
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(638, 307);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Name = "register";
+            this.Text = "register";
+            this.Load += new System.EventHandler(this.register_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox4;
+    }
+}
---
+++ PingPong/room.cs
@@ -0,0 +1,179 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net.Sockets;
+using System.Net;
+using Newtonsoft.Json.Linq;
+
+namespace PingPong
+{
+    delegate void callbackDel(string res);
+
+    public partial class room : Form
+    {
+        private const string IP = "10.156.145.184";
+        private const int PORT = 9999;
+
+
+        private static void createRoomCallback(string res)
+        {
+            //this.Hide();
+            // room2 rm2 = new room2();
+            // rm2.Show();
+        }
+
+        callbackDel CreateRoomCallBack = new callbackDel(createRoomCallback);
+
+        private string id;
+        public string passId
+        {
+            get { return this.id; }
+            set { this.id = value; }
+        }
+
+        public room()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // 방 생성 요청
+            string id = passId;
+
+            JObject json = new JObject();
+            json.Add("type", "createRoom");
+            json.Add("id", id);
+            json.Add("title", textBox1.Text);
+
+            sendJson(json.ToString());
+        }
+
+        private JObject getRoom()
+        {
+            JObject result = null;
+            JObject paramJson = new JObject();
+            paramJson.Add("type", "getRoomList");
+            result = sendJson(paramJson.ToString());
+            return result;
+        }
+
+        private void addRoomBox(JObject roomList)
+        {
+            Console.Write(roomList.ToString());
+            JArray items = (JArray)roomList["roomList"];
+            GroupBox[] boxList = new GroupBox[items.Count];
+            int i = 0;
+            foreach (JObject item in items)
+            {
+                string name = (string)item["title"];
+                int num = (int)item["room_id"];
+                boxList[i] = new GroupBox();
+                boxList[i].Location = new Point(0, i * 20);
+                boxList[i].Size = new Size(184, 76);
+                boxList[i].Name = "DText" + i.ToString();
+                boxList[i].TabIndex = i;
+                boxList[i].Click += (sender, EventArgs) => { enterRoom(sender, EventArgs, num, id);};
+                boxList[i].Text = name;
+                this.Controls.Add(boxList[i]);
+                i++;
+                /// string url = item.GetValue("url");
+            }
+
+        }
+
+        private void enterRoom (object sender, EventArgs e, int num, string id)
+        {
+            Console.Write(num);
+            JObject paramJson = new JObject();
+            paramJson.Add("type", "enterRoom");
+            paramJson.Add("room_id", num);
+            paramJson.Add("id", id);
+            sendJson(paramJson.ToString());
+        }
+
+        private JObject sendJson(String data)
+        {
+            Console.WriteLine(data);
+            JObject json = null;
+            try
+            {
+                Console.WriteLine(data);
+                IPAddress ipAdress = IPAddress.Parse(IP);
+                IPEndPoint remoteEP = new IPEndPoint(ipAdress, PORT);
+                Socket sender = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+                byte[] bytes = new byte[1024];
+                try
+                {
+
+                    sender.Connect(remoteEP);
+                    byte[] msg = Encoding.ASCII.GetBytes(data);
+                    int bytesSent = sender.Send(msg);
+                    int bytesRec = sender.Receive(bytes);
+                    var recStr = System.Text.Encoding.Default.GetString(bytes);
+                    Console.WriteLine(recStr);
+
+                    recStr = recStr.Trim();
+                    recStr = recStr.Replace("\r\n", "");
+                    json = JObject.Parse(recStr);
+                    int status = (int)json["status"];
+                    string type = (string)json["type"];
+                    if(type.Equals("enterRoom") && status == 200) {
+                        Console.Write("true");
+                        this.Hide();
+                        game gameForm = new game();
+                        gameForm.Show();
+                    }
+
+                    sender.Shutdown(SocketShutdown.Both);
+                    sender.Close();
+
+                    return json;
+                }
+                catch (Exception e)
+                {
+                    return json;
+                }
+            }
+            catch (Exception e)
+            {
+                return json;
+            }
+        }
+
+        private void FormClosing(object sender, EventArgs e)
+        {
+            Console.Write("gogogo");
+            JObject result = null;
+            JObject paramJson = new JObject();
+            paramJson.Add("type", "logout");
+            paramJson.Add("id", id);
+
+            sendJson(paramJson.ToString());
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            addRoomBox(getRoom());
+        }
+    }
+
+    public class CustomEventArgs : EventArgs
+    {
+        public string Message { get; set; }
+        public string SimpleData { get; set; }
+        public List<Object> Data { get; set; }
+    }
+
+}
